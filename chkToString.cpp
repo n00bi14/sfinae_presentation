ab379d2e0fe2ed7a18891d8795937f55a7a6f938
n00bi <kevin@hgath.de>
2017-09-17 10:39:51 +0200

    Beispiel für einen Ellipsisparameter.

--- chkToString.cpp
+++ chkToString.cpp
@@ -20,15 +20,51 @@ class B
 };
 
 
-template<class T, char (*)[sizeof(&T::toString) > 0] = nullptr> std::string ToString(T& t)
+
+
+template<typename T> class hasToString
+{
+    typedef char one;
+    typedef long two;
+
+    template<typename C> static one test(decltype(&C::toString));
+    template<typename C> static two test(...);
+
+  public:
+    enum
+    {
+      value = sizeof(test<T>(0) == sizeof(char))
+    };
+};
+
+
+template<class T, typename std::enable_if<hasToString<T>::value>::type = 0> std::string ToString(T& t)
 {
   return "Aus der Methode: " + t.toString();
 }
+
+template<class T, class Enable = void> std::string ToString(...)
+{
+  return "Ellipsis...";
+}
+
+
+
+//template<class T, char (*)[sizeof(&T::toString) > 0] = nullptr> std::string ToString(T& t)
+//{
+//  return "Aus der Methode: " + t.toString();
+//}
+
 
 
 
 int main()
 {
+  if(hasToString<A>::value)
+  {
+    A a;
+    cout << "Aus dem if: " << a.toString() << endl;
+  }
 
   A a;
   cout << ToString<A>(a) << endl;
